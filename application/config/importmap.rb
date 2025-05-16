d133201b20e9d08a788b60f49b244e2089afd53f
Aday BA <aday@theagilemonkeys.com>
2025-05-16 13:52:20 +0100

    Removed JQuery and updated dataset JS to use Stimulus (#56)

--- application/config/importmap.rb
+++ application/config/importmap.rb
@@ -5,5 +5,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.bundle.min.js"
-pin "jquery" # @3.7.1
 
