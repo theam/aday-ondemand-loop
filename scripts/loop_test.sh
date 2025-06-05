32b0a9f8efb29d433026ba5726ef939f0341978c
Aday Bujeda <aday@theagilemonkeys.com>
2025-06-05 23:30:37 +0100

    Added deploy workflow

--- scripts/loop_test.sh
+++ scripts/loop_test.sh
@@ -4,4 +4,5 @@ bundle config path --local vendor/bundle
 bundle config set build.nokogiri --use-system-libraries
 bundle config set force_ruby_platform true
 bundle install
-bundle exec rake test
+env RAILS_ENV=test bundle exec rails assets:precompile
+env RAILS_ENV=test bundle exec rake test
