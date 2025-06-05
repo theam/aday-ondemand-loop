95379c88b7001377131f06f4351533df29d34805
Aday BA <aday@theagilemonkeys.com>
2025-06-05 12:00:03 +0100

    Fixed build script (#127)

--- scripts/loop_build.sh
+++ scripts/loop_build.sh
@@ -4,6 +4,8 @@ LOOP_ROOT_URL=${APP_ROOT:-/pun/sys/loop}
 LOOP_ENV=${APP_ENV:-development}
 
 bundle config path --local vendor/bundle
+bundle config set build.nokogiri --use-system-libraries
+bundle config set force_ruby_platform true
 npm install
 bundle install
 echo "--------------------------------------------------------"
