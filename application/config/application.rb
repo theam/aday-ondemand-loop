7e67f8d5aeb94d59a4dfad574f469a210ce7332b
Aday Bujeda <aday@theagilemonkeys.com>
2025-06-15 22:59:07 +0100

    Moved connector class losing into application.rb file

--- application/config/application.rb
+++ application/config/application.rb
@@ -40,5 +40,33 @@ module DataverseForOndemand
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    connectors_root = Rails.root.join("connectors")
+
+    Dir.children(connectors_root).each do |connector|
+      connector_path = connectors_root.join(connector)
+      next unless connector_path.directory?
+
+      %w[controllers models helpers services views javascript].each do |sub|
+        sub_path = connector_path.join(sub)
+        next unless sub_path.directory?
+
+        Rails.autoloaders.main.push_dir(sub_path)
+        Rails.application.config.eager_load_paths << sub_path
+
+        case sub
+        when "views"
+          ActionController::Base.prepend_view_path(sub_path)
+        when "helpers"
+          if ActionController::Base.respond_to?(:helpers_path)
+            ActionController::Base.helpers_path << sub_path
+          end
+        when "controllers"
+          Rails.application.config.paths["app/controllers"] << sub_path
+        when "models"
+          Rails.application.config.paths["app/models"] << sub_path
+        end
+      end
+    end
   end
 end
