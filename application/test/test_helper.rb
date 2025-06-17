e46d63a3e57f3af2565162df0f7c331bb3d04dbe
Aday BA <abujeda@gmail.com>
2025-06-17 16:56:27 +0100

    chore: replace axe-core-minitest with axe-core-api

--- application/test/test_helper.rb
+++ application/test/test_helper.rb
@@ -11,7 +11,7 @@ require 'rails/test_help'
 require 'mocha/minitest'
 require 'axe/minitest'
 
-# Configure axe-core to run with WCAG level A rules by default.
+# Configure axe-core API to run with WCAG level A rules by default.
 # Change `wcag2a` to `wcag2aa` or `wcag2aaa` to audit against
 # stricter accessibility standards.
 Axe.configure do |config|
