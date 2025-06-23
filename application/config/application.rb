cd069d2ba5bd881810a8203c35651a403450707b
Aday BA <abujeda@gmail.com>
2025-06-23 10:41:34 +0100

    Fix modal title translation reference

--- application/config/application.rb
+++ application/config/application.rb
@@ -26,6 +26,9 @@ module DataverseForOndemand
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
 
+    # Load additional translations from config/locale
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locale', '**', '*.{rb,yml}').to_s]
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
