3c0106c5f902ed10cca907060a48fa9bc4033a23
Aday BA <abujeda@gmail.com>
2025-06-24 00:15:02 +0100

    Remove test parallelization for coverage

--- application/test/test_helper.rb
+++ application/test/test_helper.rb
@@ -40,8 +40,7 @@ require 'mocha/minitest'
 
 module ActiveSupport
   class TestCase
-    # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
+    # Run tests sequentially to preserve accurate coverage metrics
 
     # Add more helper methods to be used by all tests here...
     include FileFixtureHelper
