434424bb71de47eac31813255188e20a33490145
Aday Bujeda <aday@theagilemonkeys.com>
2025-06-25 15:36:18 +0100

    fix: Added DV items into the Dataverse landing page controller test

--- application/test/controllers/dataverse/landing_page_controller_test.rb
+++ application/test/controllers/dataverse/landing_page_controller_test.rb
@@ -2,7 +2,17 @@ require 'test_helper'
 
 class Dataverse::LandingPageControllerTest < ActionDispatch::IntegrationTest
   test 'index loads installations' do
-    registry = mock('reg'); registry.stubs(:installations).returns([])
+    dvs = [{
+      id: 'dv-test-01',
+      name: 'DV Test 01',
+      hostname: 'https://dv-01.org',
+    },
+     {
+       id: 'dv-test-02',
+       name: 'DV Test 02',
+       hostname: 'https://dv-02.org',
+     }]
+    registry = mock('reg'); registry.stubs(:installations).returns(dvs)
     DataverseHubRegistry.stubs(:registry).returns(registry)
     get view_dataverse_landing_url
     assert_response :success
