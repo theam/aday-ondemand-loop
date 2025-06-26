4b2f4c827f131d20cd218cb7ef2e2bbd7b6ffbed
Aday BA <abujeda@gmail.com>
2025-06-26 14:29:56 +0100

    Add controller locales and update translation paths

--- application/app/controllers/dataverse/landing_page_controller.rb
+++ application/app/controllers/dataverse/landing_page_controller.rb
@@ -9,7 +9,7 @@ class Dataverse::LandingPageController < ApplicationController
       @installations_page = Page.new(installations, page, 25)
     rescue Exception => e
       log_error('Dataverse Installations service error', {}, e)
-      flash[:alert] = t(".dataverse_installations_service_error")
+      flash[:alert] = t("dataverse.landing_page.index.dataverse_installations_service_error")
       redirect_to root_path
     end
   end
