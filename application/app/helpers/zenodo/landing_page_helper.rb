6cd5d697cc7065b566ef5c224f59464a3863665c
Aday Bujeda <aday@theagilemonkeys.com>
2025-06-27 10:55:09 +0100

    fix: Fixed landing page style after review

--- application/app/helpers/zenodo/landing_page_helper.rb
+++ application/app/helpers/zenodo/landing_page_helper.rb
@@ -4,7 +4,9 @@ module Zenodo::LandingPageHelper
     html_options['aria-label'] = I18n.t('acts_as_page.link_prev_page_a11y_label')
     html_options[:title] = I18n.t('acts_as_page.link_prev_page_title')
     html_options[:class] = [html_options[:class], 'btn btn-sm btn-outline-dark'].compact.join(' ')
-    link_to('<', view_zenodo_landing_path(query: query, page: search_result.prev_page), html_options)
+    link_to(view_zenodo_landing_path(query: query, page: search_result.prev_page), html_options) do
+      raw('<i class="bi bi-chevron-left"></i>')
+    end
   end
 
   def link_to_search_next_page(query, search_result, html_options = {})
@@ -12,7 +14,9 @@ module Zenodo::LandingPageHelper
     html_options['aria-label'] = I18n.t('acts_as_page.link_next_page_a11y_label')
     html_options[:title] = I18n.t('acts_as_page.link_next_page_title')
     html_options[:class] = [html_options[:class], 'btn btn-sm btn-outline-dark'].compact.join(' ')
-    link_to('>', view_zenodo_landing_path(query: query, page: search_result.next_page), html_options)
+    link_to(view_zenodo_landing_path(query: query, page: search_result.next_page), html_options) do
+      raw('<i class="bi bi-chevron-right"></i>')
+    end
   end
 end
 
