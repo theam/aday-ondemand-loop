e3c25cc0264ef473f5ed5f4fd7c916ec2f78dc9e
Aday BA <abujeda@gmail.com>
2025-07-04 17:35:00 +0100

    Fix docs build path

--- scripts/user_guide.sh
+++ scripts/user_guide.sh
@@ -7,4 +7,7 @@ set -e
 pip install --quiet mkdocs mkdocs-material
 
 # Build the documentation
-mkdocs build -f docs/user_guide/mkdocs.yml --site-dir site
+# Use an absolute path for the output directory so MkDocs does not
+# treat it as a subdirectory of the documentation folder.
+SITE_DIR="$(pwd)/site"
+mkdocs build -f docs/user_guide/mkdocs.yml --site-dir "$SITE_DIR"
