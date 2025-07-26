e41e316f1170dca233f5b0c3d369c771120bfa09
Aday BA <abujeda@gmail.com>
2025-07-26 22:08:13 +0100

    Refactor Dataverse URL building

--- application/app/services/dataverse/concerns/dataverse_url_builder.rb
+++ application/app/services/dataverse/concerns/dataverse_url_builder.rb
@@ -1,20 +1,35 @@
 module Dataverse::Concerns::DataverseUrlBuilder
   extend ActiveSupport::Concern
 
-  def collection_url
-    raise 'collection_id is missing' unless collection_id
+  # Module-level helpers to build URLs from components
+  def collection_url(dataverse_url, id)
     FluentUrl.new(dataverse_url)
       .add_path('dataverse')
-      .add_path(collection_id)
+      .add_path(id)
       .to_s
   end
+  module_function :collection_url
 
-  def dataset_url(version: nil)
-    raise 'dataset_id (DOI) is missing' unless dataset_id
+  def dataset_url(dataverse_url, id, version: nil)
     url = FluentUrl.new(dataverse_url)
               .add_path('dataset.xhtml')
-              .add_param('persistentId', dataset_id)
+              .add_param('persistentId', id)
     url.add_param('version', version) if version
     url.to_s
+  end
+  module_function :dataset_url
+
+  included do
+    def collection_url(id = nil)
+      id ||= respond_to?(:collection_id) ? collection_id : nil
+      raise 'collection_id is missing' unless id
+      Dataverse::Concerns::DataverseUrlBuilder.collection_url(dataverse_url, id)
+    end
+
+    def dataset_url(id = nil, version: nil)
+      id ||= respond_to?(:dataset_id) ? dataset_id : nil
+      raise 'dataset_id (DOI) is missing' unless id
+      Dataverse::Concerns::DataverseUrlBuilder.dataset_url(dataverse_url, id, version: version)
+    end
   end
 end
