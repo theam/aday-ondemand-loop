c17d822e7550303d235865dcf005202a7e581c3e
Aday Bujeda <aday@theagilemonkeys.com>
2025-08-05 15:38:46 +0100

    Making Zenodo::ProjectService DRY

--- application/app/services/zenodo/project_service.rb
+++ application/app/services/zenodo/project_service.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Zenodo
   class ProjectService
     include DateTimeCommon
@@ -13,52 +15,46 @@ module Zenodo
     end
 
     def create_files_from_record(project, record, file_ids)
-      record_files = record.files.select { |f| file_ids.include?(f.id) }
-      record_files.map do |record_file|
-        DownloadFile.new.tap do |f|
-          f.id = DownloadFile.generate_id
-          f.project_id = project.id
-          f.creation_date = now
-          f.type = ConnectorType::ZENODO
-          f.filename = record_file.filename
-          f.status = FileStatus::PENDING
-          f.size = record_file.filesize
-          f.metadata = {
-            zenodo_url: @zenodo_url,
-            type: 'records',
-            type_id: record.id,
-            id: record_file.id,
-            download_url: record_file.download_url,
-            temp_location: nil
-          }
-          @file_utils.make_download_file_unique(f)
-        end
-      end
+      build_download_files(
+        project: project,
+        source: record,
+        file_ids: file_ids,
+        type: 'records'
+      )
     end
 
     def create_files_from_deposition(project, deposition, file_ids)
-      files = deposition.files.select { |f| file_ids.include?(f.id) }
-      files.map do |deposition_file|
+      build_download_files(
+        project: project,
+        source: deposition,
+        file_ids: file_ids,
+        type: 'depositions'
+      )
+    end
+
+    private
+
+    def build_download_files(project:, source:, file_ids:, type:)
+      source.files.select { |f| file_ids.include?(f.id) }.map do |file|
         DownloadFile.new.tap do |f|
           f.id = DownloadFile.generate_id
           f.project_id = project.id
           f.creation_date = now
           f.type = ConnectorType::ZENODO
-          f.filename = deposition_file.filename
+          f.filename = file.filename
           f.status = FileStatus::PENDING
-          f.size = deposition_file.filesize
+          f.size = file.filesize
           f.metadata = {
             zenodo_url: @zenodo_url,
-            type: 'depositions',
-            type_id: deposition.id,
-            id: deposition_file.id,
-            download_url: deposition_file.download_url,
+            type: type,
+            type_id: source.id,
+            id: file.id,
+            download_url: file.download_url,
             temp_location: nil
           }
           @file_utils.make_download_file_unique(f)
         end
       end
     end
-
   end
 end
