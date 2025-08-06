9c8f5127b10b837282c0dc684c769dec71251dd9
Aday BA <abujeda@gmail.com>
2025-08-06 13:36:31 +0100

    test: stub connector actions in explore controller tests

--- application/test/controllers/explore_controller_test.rb
+++ application/test/controllers/explore_controller_test.rb
@@ -1,78 +1,75 @@
 require "test_helper"
 
 class ExploreControllerTest < ActionDispatch::IntegrationTest
-  test 'should render placeholder template with notice' do
+  def stub_processor(action, result)
+    processor = mock("ExploreProcessor")
+    processor.stubs(:params_schema).returns(%i[connector_type server_domain object_type object_id query server_scheme server_port])
+    processor.expects(action).with(kind_of(Hash)).returns(result)
+    ConnectorClassDispatcher.stubs(:explore_connector_processor).returns(processor)
+  end
+
+  test "landing action renders template when processor succeeds" do
+    stub_processor(:landing, ConnectorResult.new(template: "/sitemap/index", locals: {}, success: true))
+
+    get explore_landing_url(connector_type: "zenodo")
+
+    assert_response :success
+  end
+
+  test "landing action redirects with message when processor fails" do
+    stub_processor(:landing, ConnectorResult.new(message: { alert: "error" }, success: false))
+
+    get explore_landing_url(connector_type: "zenodo")
+
+    assert_redirected_to root_path
+    assert_equal "error", flash[:alert]
+  end
+
+  test "redirects when repo url is invalid" do
+    get "/explore/zenodo/%20/records/1"
+
+    assert_redirected_to root_path
+    assert_equal I18n.t("explore.show.message_invalid_repo_url", repo_url: ""), flash[:alert]
+  end
+
+  test "show action renders template when processor succeeds" do
+    stub_processor(:show, ConnectorResult.new(template: "/sitemap/index", locals: {}, success: true))
+
     get explore_url(
-      connector_type: 'dataverse',
-      server_domain: 'dataverse.harvard.edu',
-      object_type: 'dataverse',
-      object_id: 'harvard'
+      connector_type: "zenodo",
+      server_domain: "example.org",
+      object_type: "records",
+      object_id: "1"
     )
 
     assert_response :success
-    assert_select 'div.alert-info'
   end
 
-  test 'zenodo landing action is not supported' do
-    get explore_landing_url(
-      connector_type: 'zenodo',
-      query: 'test'
-    )
-
-    assert_redirected_to root_path
-    assert_equal I18n.t('connectors.zenodo.actions.landing.message_action_not_supported'), flash[:alert]
-  end
-
-  test 'redirects when repo url is invalid' do
-    get '/explore/zenodo/%20/actions/landing'
-
-    assert_redirected_to root_path
-    assert_equal I18n.t('explore.show.message_invalid_repo_url', repo_url: ''), flash[:alert]
-  end
-
-  test 'zenodo records action renders record view' do
-    record = Struct.new(:id, :title, :description, :publication_date, :files) do
-      def draft?
-        false
-      end
-    end.new('1', 'rec', '', '', [])
-    Zenodo::RecordService.any_instance.expects(:find_record).with('1').returns(record)
-    user_settings_mock = mock('UserSettings')
-    user_settings_mock.stubs(:user_settings).returns(OpenStruct.new(active_project: nil))
-    Current.stubs(:settings).returns(user_settings_mock)
+  test "show action redirects with message when processor fails" do
+    stub_processor(:show, ConnectorResult.new(message: { alert: "error" }, success: false))
 
     get explore_url(
-      connector_type: 'zenodo',
-      server_domain: 'zenodo.org',
-      object_type: 'records',
-      object_id: '1'
+      connector_type: "zenodo",
+      server_domain: "example.org",
+      object_type: "records",
+      object_id: "1"
     )
 
-    assert_response :success
-    assert_select 'h2', text: 'rec'
+    assert_redirected_to root_path
+    assert_equal "error", flash[:alert]
   end
 
-  test 'zenodo records download redirects with notice' do
-    record = Struct.new(:id, :title, :description, :publication_date, :files) do
-      def draft?
-        false
-      end
-    end.new('1', 'rec', '', '', [])
-    Zenodo::RecordService.any_instance.stubs(:find_record).returns(record)
-    Project.stubs(:find).returns(nil)
-    project = Project.new(name: 'P')
-    project.stubs(:save).returns(true)
-    Zenodo::ProjectService.any_instance.stubs(:initialize_project).returns(project)
-    Zenodo::ProjectService.any_instance.stubs(:create_files_from_record).returns([])
+  test "create action redirects with message from processor" do
+    stub_processor(:create, ConnectorResult.new(message: { notice: "ok" }, success: true))
 
     post explore_url(
-      connector_type: 'zenodo',
-      server_domain: 'zenodo.org',
-      object_type: 'records',
-      object_id: '1'
+      connector_type: "zenodo",
+      server_domain: "example.org",
+      object_type: "records",
+      object_id: "1"
     )
 
     assert_redirected_to root_path
-    assert_equal I18n.t('zenodo.records.message_success', files: 0, project_name: 'P'), flash[:notice]
+    assert_equal "ok", flash[:notice]
   end
 end
