76c837aa6fc2b00dcc9606dc424d2316e1e393c8
Aday BA <abujeda@gmail.com>
2025-08-07 22:10:22 +0100

    Avoid DOI re-resolution when object URL exists

--- application/test/services/repo/resolvers/doi_resolver_test.rb
+++ application/test/services/repo/resolvers/doi_resolver_test.rb
@@ -27,4 +27,32 @@ class Repo::Resolvers::DoiResolverTest < ActiveSupport::TestCase
     @resolver.resolve(context)
     assert_nil context.object_url
   end
+
+  test 'resolve returns when object_url already set' do
+    client = HttpClientMock.new(file_path: fixture_path('downloads/basic_http/sample_utf8.txt'))
+    context = Repo::RepoResolverContext.new('doi:10.123/abc', http_client: client)
+    context.object_url = 'https://already.com'
+    @resolver.resolve(context)
+    assert_equal 'https://already.com', context.object_url
+    refute client.called?
+  end
+
+  test 'resolve handles doi.org urls directly' do
+    client = HttpClientMock.new(file_path: fixture_path('downloads/basic_http/sample_utf8.txt'),
+                                status_code: 302,
+                                headers: { 'location' => 'https://example.com' })
+    context = Repo::RepoResolverContext.new('https://doi.org/10.123/abc', http_client: client)
+    @resolver.resolve(context)
+    assert_equal 'https://example.com', context.object_url
+    assert_equal 'https://doi.org/10.123/abc', client.called_path
+  end
+
+  test 'resolve sets object_url to input for non doi urls' do
+    client = HttpClientMock.new(file_path: fixture_path('downloads/basic_http/sample_utf8.txt'))
+    url = 'https://example.org/resource'
+    context = Repo::RepoResolverContext.new(url, http_client: client)
+    @resolver.resolve(context)
+    assert_equal url, context.object_url
+    refute client.called?
+  end
 end
