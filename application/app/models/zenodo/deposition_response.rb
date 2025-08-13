9a7482000aff1c6f1603c67c19ec0d35f926a2ca
Aday BA <abujeda@gmail.com>
2025-08-13 15:44:36 +0100

    refactor: use ActiveSupport delegate for deposition response

--- application/app/models/zenodo/deposition_response.rb
+++ application/app/models/zenodo/deposition_response.rb
@@ -1,17 +1,12 @@
 # frozen_string_literal: true
 
-require 'forwardable'
-require_relative 'deposition'
-
 module Zenodo
   class DepositionResponse
-    extend Forwardable
-
     attr_reader :deposition
 
-    def_delegators :@deposition, :id, :title, :description, :publication_date,
-                                     :files, :file_count, :bucket_url, :submitted,
-                                     :raw, :draft?, :to_s
+    delegate :id, :title, :description, :publication_date,
+             :files, :file_count, :bucket_url, :submitted,
+             :raw, :draft?, :to_s, to: :deposition
 
     def initialize(response_body)
       @deposition = Deposition.new(JSON.parse(response_body))
