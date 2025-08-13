6ffad3143f435adb55faa79d912414c17bfa55d3
Aday BA <abujeda@gmail.com>
2025-08-13 15:06:40 +0100

    Refactor Zenodo deposition response to use composition

--- application/app/models/zenodo/deposition_response.rb
+++ application/app/models/zenodo/deposition_response.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
+require 'forwardable'
 require_relative 'deposition'
 
 module Zenodo
-  class DepositionResponse < Deposition
+  class DepositionResponse
+    extend Forwardable
+
+    attr_reader :deposition
+
+    def_delegators :@deposition, :id, :title, :description, :publication_date,
+                                     :files, :file_count, :bucket_url, :submitted,
+                                     :raw, :draft?, :to_s
+
     def initialize(response_body)
-      super(JSON.parse(response_body))
+      @deposition = Deposition.new(JSON.parse(response_body))
     end
   end
 end
