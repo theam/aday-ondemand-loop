5c3dd3483736a3bb46efd56bf9f7e9bdfd9a88fc
Aday BA <abujeda@gmail.com>
2025-08-18 00:04:32 +0100

    feat: add submission spinner to inline field editor

--- application/app/javascript/controllers/update_inline_field_controller.js
+++ application/app/javascript/controllers/update_inline_field_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 import { showFlash } from 'utils/flash_message'
 
 export default class extends Controller {
-  static targets = ["display", "name", "form", "input"]
+  static targets = ["display", "name", "form", "input", "label", "spinner"]
   static values = {
     fieldName: String,
     initialValue: String,
@@ -49,6 +49,7 @@ export default class extends Controller {
 
   submitJson(path, payload, method) {
     const csrfToken = window.loop_app_config.csrf_token
+    this.showSpinner()
     fetch(path, {
       method: method,
       headers: {
@@ -75,9 +76,13 @@ export default class extends Controller {
         const message = this.hasErrorMessageValue ? this.errorMessageValue : 'Could not update field.'
         showFlash("error", message)
       })
+      .finally(() => {
+        this.hideSpinner()
+      })
   }
 
   submitForm(path, payload, method) {
+    this.showSpinner()
     const form = document.createElement("form")
     form.method = "POST"
     form.action = path
@@ -105,7 +110,11 @@ export default class extends Controller {
     })
 
     document.body.appendChild(form)
-    form.submit()
+
+    const uiDelay = window.loop_app_config.ui_feedback_delay
+    setTimeout(() => {
+      form.submit()
+    }, uiDelay)
   }
 
   cancel() {
@@ -114,5 +123,26 @@ export default class extends Controller {
     if (this.hasInitialValueValue) {
       this.inputTarget.value = this.initialValueValue
     }
+  }
+
+  showSpinner() {
+    const { width, height } = this.formTarget.getBoundingClientRect()
+    this.formTarget.style.width = `${width}px`
+    this.formTarget.style.height = `${height}px`
+    this.spinnerTarget.classList.remove("d-none")
+    this.spinnerTarget.setAttribute('aria-busy', 'true')
+    this.labelTargets.forEach(el => el.classList.add("d-none"))
+  }
+
+  hideSpinner() {
+    const uiDelay = window.loop_app_config.ui_feedback_delay
+
+    setTimeout(() => {
+      this.labelTargets.forEach(el => el.classList.remove("d-none"))
+      this.spinnerTarget.classList.add("d-none")
+      this.formTarget.style.width = ""
+      this.formTarget.style.height = ""
+      this.spinnerTarget.setAttribute('aria-busy', 'false')
+    }, uiDelay)
   }
 }
