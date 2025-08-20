a3f6a158071d41638acfe23f9e7596203063d50b
Aday BA <abujeda@gmail.com>
2025-08-20 13:40:41 +0100

    Add project selector to repo resolver bar

--- application/test/views/layouts/repo_resolver_bar_view_test.rb
+++ application/test/views/layouts/repo_resolver_bar_view_test.rb
@@ -3,6 +3,15 @@
 require 'test_helper'
 
 class RepoResolverBarViewTest < ActionView::TestCase
+  setup do
+    I18n.backend.store_translations(:en, project_selection: {
+      project_select_label: 'Choose an existing project',
+      button_open_project_title: 'Open selected project details page',
+      button_open_project_label: 'Open selected project'
+    })
+    view.stubs(:select_project_list).returns([])
+    view.stubs(:select_project_list_name).returns('')
+  end
   test 'defaults url to repo_resolver_path' do
     html = render partial: 'layouts/repo_resolver_bar', locals: { show_images: false }
     assert_includes html, "action=\"#{repo_resolver_path}\""
@@ -12,5 +21,17 @@ class RepoResolverBarViewTest < ActionView::TestCase
     custom_url = '/custom/path'
     html = render partial: 'layouts/repo_resolver_bar', locals: { url: custom_url, show_images: false }
     assert_includes html, "action=\"#{custom_url}\""
+  end
+
+  test 'renders project selector row' do
+    project = Project.new(id: '1', name: 'Project One')
+    view.stubs(:select_project_list).returns([project])
+    view.stubs(:select_project_list_name).returns(project.name)
+
+    html = render partial: 'layouts/repo_resolver_bar', locals: { show_images: false }
+
+    assert_includes html, '<hr'
+    assert_includes html, "<option value=\"#{project.id}\""
+    assert_includes html, 'btn btn-sm btn-outline-secondary'
   end
 end
