0cd8a278b89d79b434e3618d78eba44398425ad3
Aday BA <abujeda@gmail.com>
2025-08-21 11:26:26 +0100

    feat: support form submission in project selector

--- application/app/javascript/controllers/select_project_controller.js
+++ application/app/javascript/controllers/select_project_controller.js
@@ -4,6 +4,7 @@ import { showFlash } from 'utils/flash_message'
 
 export default class extends Controller {
     static targets = ['displayLabel', 'spinner']
+    static values = { type: String }
 
     connect() {
         // DELAY UPDATE TO ALLOW OTHER CONTROLLERS TO CONNECT AND ADD LISTENERS
@@ -26,27 +27,32 @@ export default class extends Controller {
         const projectPath = link.dataset.projectPath
 
         if (projectId !== this.selectedProjectId) {
+            const requestType = this.hasTypeValue ? this.typeValue : 'json'
             this.showSpinner()
-            this.setActiveProject(projectPath)
-                .then(() => {
-                    this.selectedProjectId = projectId
-                    this.updateDisplay(link)
-                    this.dispatchSelectedProject(projectId, projectName, projectPath)
-                })
-                .catch(error => {
-                    const message = error?.error ?? window.loop_app_config.i18n.generic_server_error
-                    showFlash('error', message)
-                })
-                .finally(() => {
-                    this.hideSpinner()
-                })
+            if (requestType === 'form') {
+                this.submitForm(projectPath)
+            } else {
+                this.submitJson(projectPath)
+                    .then(() => {
+                        this.selectedProjectId = projectId
+                        this.updateDisplay(link)
+                        this.dispatchSelectedProject(projectId, projectName, projectPath)
+                    })
+                    .catch(error => {
+                        const message = error?.error ?? window.loop_app_config.i18n.generic_server_error
+                        showFlash('error', message)
+                    })
+                    .finally(() => {
+                        this.hideSpinner()
+                    })
+            }
         } else {
             this.updateDisplay(link)
             this.dispatchSelectedProject(projectId, projectName, projectPath)
         }
     }
 
-    setActiveProject(projectPath) {
+    submitJson(projectPath) {
         const csrfToken = window.loop_app_config.csrf_token
         return fetch(`${projectPath}/set_active`, {
             method: 'POST',
@@ -59,6 +65,35 @@ export default class extends Controller {
         }).then(response => {
             if (!response.ok) return response.json().then(data => Promise.reject(data))
         })
+    }
+
+    submitForm(projectPath) {
+        const path = `${projectPath}/set_active`
+        const form = document.createElement('form')
+        form.method = 'POST'
+        form.action = path
+
+        const csrfInput = document.createElement('input')
+        csrfInput.type = 'hidden'
+        csrfInput.name = 'authenticity_token'
+        csrfInput.value = window.loop_app_config.csrf_token
+        form.appendChild(csrfInput)
+
+        const hash = window.location.hash
+        if (hash && hash.length > 1) {
+            const anchorInput = document.createElement('input')
+            anchorInput.type = 'hidden'
+            anchorInput.name = 'anchor'
+            anchorInput.value = hash.substring(1)
+            form.appendChild(anchorInput)
+        }
+
+        document.body.appendChild(form)
+
+        const uiDelay = window.loop_app_config.ui_feedback_delay
+        setTimeout(() => {
+            form.submit()
+        }, uiDelay)
     }
 
     updateDisplay(link) {
