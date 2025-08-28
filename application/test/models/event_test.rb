c923525f7b5a0ac079340d7ebea141584a08e234
David Verdú <davidverdu@theagilemonkeys.com>
2025-08-28 18:35:16 +0100

    fix tests

--- application/test/models/event_test.rb
+++ application/test/models/event_test.rb
@@ -6,7 +6,7 @@ class EventTest < ActiveSupport::TestCase
     Project.stubs(:metadata_root_directory).returns(@tmp_dir)
     @project = Project.new(id: 'proj1', name: 'Proj', download_dir: '/tmp/proj')
     @project.save
-    @event = Event.new(project_id: @project.id, id: 'evt1', type: EventType::GENERIC, metadata: { 'foo' => 'bar' }, creation_date: DateTimeCommon.now)
+    @event = Event.new(project_id: @project.id, entity_id: @project.id, entity_type: 'project', type: 'project_created', metadata: { 'foo' => 'bar' }, creation_date: DateTimeCommon.now)
     @events_file = Project.events_file(@project.id)
   end
 
@@ -15,19 +15,19 @@ class EventTest < ActiveSupport::TestCase
   end
 
   test 'save appends event to file' do
-    assert @event.save
+    @event.save
     assert File.exist?(@events_file), 'events file not created'
     data = YAML.safe_load(File.read(@events_file))
     assert_equal 2, data.size
-    assert_equal 'evt1', data.last['id']
+    assert_equal @project.id, data.last['entity_id']
   end
 
   test 'for_project returns stored events' do
     @event.save
     events = Event.for_project(@project.id)
     assert_equal 2, events.size
-    assert_equal 'evt1', events.last.id
-    assert_equal EventType::GENERIC, events.last.type
+    assert_equal @project.id, events.last.entity_id
+    assert_equal 'project_created', events.last.type
   end
 
   test 'for_project handles legacy hash file' do
@@ -35,27 +35,7 @@ class EventTest < ActiveSupport::TestCase
     File.open(@events_file, 'w') { |f| f.write(@event.to_h.to_yaml) }
     events = Event.for_project(@project.id)
     assert_equal 1, events.size
-    assert_equal 'evt1', events.first.id
+    assert_equal @project.id, events.first.entity_id
   end
 
-  test 'download file created event sets type and metadata' do
-    event = Events::DownloadFileCreated.new(project_id: @project.id, file_id: 'f1', filename: 'test.txt', file_size: 10)
-    assert_equal EventType::DOWNLOAD_FILE_CREATED, event.type
-    assert_equal 'f1', event.metadata['file_id']
-    assert_equal 'test.txt', event.metadata['filename']
-  end
-
-  test 'project created event sets type and metadata' do
-    event = Events::ProjectCreated.new(project_id: @project.id, project_name: 'Proj')
-    assert_equal EventType::PROJECT_CREATED, event.type
-    assert_equal 'Proj', event.metadata['project_name']
-    assert_equal 1, event.metadata.size
-  end
-
-  test 'project updated event sets type and metadata' do
-    event = Events::ProjectUpdated.new(project_id: @project.id, project_name: 'Proj')
-    assert_equal EventType::PROJECT_UPDATED, event.type
-    assert_equal 'Proj', event.metadata['project_name']
-    assert_equal 1, event.metadata.size
-  end
 end
