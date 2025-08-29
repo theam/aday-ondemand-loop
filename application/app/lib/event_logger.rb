5dd2c4e9794e6cacb9f8c9bab09aa7200e1ffaa2
David Verdú <davidverdu@theagilemonkeys.com>
2025-08-29 16:04:31 +0100

    enhance event_logger module

--- application/app/lib/event_logger.rb
+++ application/app/lib/event_logger.rb
@@ -2,15 +2,11 @@
 
 module EventLogger
   def record_event(event)
-    EventLogger.record_event(event)
-  end
-
-  module_function
-
-  def record_event(event)
     event.save
   rescue => e
     LoggingCommon.log_error('Cannot record event', { event_class: event.class.name }, e)
     false
   end
+
+  module_function :record_event
 end
