446a15c7b52eb7c8d14fb24a1f1a09f879b80469
Aday BA <abujeda@gmail.com>
2025-08-31 20:42:12 +0100

    Load OOD credentials into Cypress config

--- automated_tests/cypress.config.js
+++ automated_tests/cypress.config.js
@@ -1,9 +1,15 @@
 const { defineConfig } = require('cypress');
+const loadCredentials = require('./read_credentials');
+const { username, password } = loadCredentials();
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://ood',
     chromeWebSecurity: false,
     video: false,
+  },
+  env: {
+    ood_username: username,
+    ood_password: password,
   },
 });
--- automated_tests/cypress/e2e/homepage.cy.js
+++ automated_tests/cypress/e2e/homepage.cy.js
@@ -1,6 +1,11 @@
 describe('Home page', () => {
   it('shows welcome and beta notices on first visit', () => {
-    cy.visit('/');
+    cy.visit('/', {
+      auth: {
+        username: Cypress.env('ood_username'),
+        password: Cypress.env('ood_password'),
+      },
+    });
     cy.location('pathname').should('eq', '/');
     cy.contains('Welcome to OnDemand Loop!').should('be.visible');
     cy.contains('Beta Notice').should('be.visible');
