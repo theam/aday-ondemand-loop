1fb166b6a6c43cac8a75b9d29eb0d37297f32977
Aday Bujeda <aday@theagilemonkeys.com>
2025-09-02 14:35:52 +0100

    Refactoring

--- .github/scripts/wait_for_loop.sh
+++ .github/scripts/wait_for_loop.sh
@@ -4,27 +4,16 @@ set -euo pipefail
 ENDPOINT="${ENDPOINT:-https://localhost:22200/pun/sys/loop}"
 OOD_USERNAME="${OOD_USERNAME:-ood}"
 OOD_PASSWORD="${OOD_PASSWORD:-ood}"
-MAX_RETRIES="${MAX_RETRIES:-20}"
-SLEEP_SECS="${SLEEP_SECS:-25}"
-
-echo "Trying: ${ENDPOINT} with username: ${OOD_USERNAME}"
+MAX_RETRIES="${MAX_RETRIES:-10}"
+SLEEP_SECS="${SLEEP_SECS:-10}"
 
 docker ps
 
-for i in {1..10}; do
-  if curl -sk -u "ood:ood" https://localhost:22200/pun/sys/loop > /dev/null; then
-    echo "✅ Service is up"
-    break
-  fi
-  echo "⏳ Waiting for service (attempt $i)..."
-  sleep 5
-done
-
-docker exec test_loop_ood cat /var/log/ondemand-nginx/ood/error.log
+echo "Trying: ${ENDPOINT} with username: ${OOD_USERNAME}"
 
 for ((i=1; i<=MAX_RETRIES; i++)); do
   STATUS_RECEIVED=$(curl -k -s -o /dev/null -L -w '%{http_code}' \
-    -u "${OOD_USERNAME}:${OOD_PASSWORD}" "${ENDPOINT}")
+    -u "${OOD_USERNAME}:${OOD_PASSWORD}" "${ENDPOINT}" || echo "000")
 
   if [ "${STATUS_RECEIVED}" = "200" ]; then
     echo "OnDemand Loop up and running..."
@@ -37,5 +26,6 @@ for ((i=1; i<=MAX_RETRIES; i++)); do
   sleep "${SLEEP_SECS}"
 done
 
+docker exec test_loop_ood cat /var/log/ondemand-nginx/ood/error.log
 echo "OnDemand Loop not running after ${MAX_RETRIES} attempts..."
 exit 1
