15a92fba4ab9421fa78a1c9f571bb6c45ae661a5
Aday BA <aday@theagilemonkeys.com>
2025-09-09 15:13:05 +0100

    feat: Added a independent homepage (#454)

--- application/config/routes.rb
+++ application/config/routes.rb
@@ -71,5 +71,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "projects#index"
+  root "home#index"
 end
