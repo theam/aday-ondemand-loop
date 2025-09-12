98cdd34f6505027cc3f5304429080c29f9869b1e
Aday Bujeda <aday@theagilemonkeys.com>
2025-09-12 10:11:35 +0100

    feat: disable UploadBundle add files button if the dataset not ready for upload

--- application/test/connectors/dataverse/upload_bundle_connector_metadata_test.rb
+++ application/test/connectors/dataverse/upload_bundle_connector_metadata_test.rb
@@ -53,4 +53,154 @@ class Dataverse::UploadBundleConnectorMetadataTest < ActiveSupport::TestCase
     refute meta.api_key?
     assert meta.api_key_required?
   end
+
+  test 'method_missing returns nil for undefined methods' do
+    result = @meta.undefined_method
+    assert_nil result
+  end
+
+  test 'method_missing returns nil for undefined methods with arguments' do
+    result = @meta.undefined_method('arg1', 'arg2')
+    assert_nil result
+  end
+
+  test 'repo_name returns dataverse_url' do
+    expected_url = 'https://demo.dataverse.org'
+    assert_equal expected_url, @meta.repo_name
+  end
+
+  test 'configured? returns true when all required fields are present' do
+    @bundle.metadata = {
+      dataverse_url: 'https://demo.dataverse.org',
+      collection_title: 'My Collection',
+      dataset_title: 'My Dataset',
+      collection_id: 'COL1',
+      dataset_id: 'DS1',
+      auth_key: 'KEY'
+    }
+    meta = Dataverse::UploadBundleConnectorMetadata.new(@bundle)
+    assert meta.configured?
+  end
+
+  test 'configured? returns false when api_key is missing' do
+    @bundle.metadata.delete(:auth_key)
+    ::Configuration.repo_db.stubs(:get).returns(nil)
+    meta = Dataverse::UploadBundleConnectorMetadata.new(@bundle)
+    refute meta.configured?
+  end
+
+  test 'configured? returns false when dataset_id is missing' do
+    @bundle.metadata[:dataset_id] = nil
+    meta = Dataverse::UploadBundleConnectorMetadata.new(@bundle)
+    refute meta.configured?
+  end
+
+  test 'configured? returns false when dataset_title is missing' do
+    @bundle.metadata[:dataset_title] = nil
+    meta = Dataverse::UploadBundleConnectorMetadata.new(@bundle)
+    refute meta.configured?
+  end
+
+  test 'configured? returns false when collection_id is missing' do
+    @bundle.metadata[:collection_id] = nil
+    meta = Dataverse::UploadBundleConnectorMetadata.new(@bundle)
+    refute meta.configured?
+  end
+
+  test 'external_url returns dataset URL for draft version' do
+    @bundle.metadata[:dataset_id] = 'DS123'
+    meta = Dataverse::UploadBundleConnectorMetadata.new(@bundle)
+    meta.stubs(:dataset_url).with(version: 'draft').returns('https://demo.dataverse.org/dataset.xhtml?persistentId=DS123&version=DRAFT')
+    
+    expected_url = 'https://demo.dataverse.org/dataset.xhtml?persistentId=DS123&version=DRAFT'
+    assert_equal expected_url, meta.external_url
+  end
+
+  test 'fetch_draft? returns true when api_key present, dataset_id present, and dataset_title nil' do
+    @bundle.metadata = {
+      dataverse_url: 'https://demo.dataverse.org',
+      dataset_id: 'DS123',
+      dataset_title: nil,
+      auth_key: 'KEY'
+    }
+    meta = Dataverse::UploadBundleConnectorMetadata.new(@bundle)
+    assert meta.fetch_draft?
+  end
+
+  test 'fetch_draft? returns false when api_key is missing' do
+    @bundle.metadata = {
+      dataverse_url: 'https://demo.dataverse.org',
+      dataset_id: 'DS123',
+      dataset_title: nil
+    }
+    ::Configuration.repo_db.stubs(:get).returns(nil)
+    meta = Dataverse::UploadBundleConnectorMetadata.new(@bundle)
+    refute meta.fetch_draft?
+  end
+
+  test 'fetch_draft? returns false when dataset_id is missing' do
+    @bundle.metadata = {
+      dataverse_url: 'https://demo.dataverse.org',
+      dataset_id: nil,
+      dataset_title: nil,
+      auth_key: 'KEY'
+    }
+    meta = Dataverse::UploadBundleConnectorMetadata.new(@bundle)
+    refute meta.fetch_draft?
+  end
+
+  test 'fetch_draft? returns false when dataset_title is present' do
+    @bundle.metadata = {
+      dataverse_url: 'https://demo.dataverse.org',
+      dataset_id: 'DS123',
+      dataset_title: 'My Dataset',
+      auth_key: 'KEY'
+    }
+    meta = Dataverse::UploadBundleConnectorMetadata.new(@bundle)
+    refute meta.fetch_draft?
+  end
+
+  test 'select_collection? returns true when api_key present, collection_id nil, and dataset_id nil' do
+    @bundle.metadata = {
+      dataverse_url: 'https://demo.dataverse.org',
+      collection_id: nil,
+      dataset_id: nil,
+      auth_key: 'KEY'
+    }
+    meta = Dataverse::UploadBundleConnectorMetadata.new(@bundle)
+    assert meta.select_collection?
+  end
+
+  test 'select_collection? returns false when api_key is missing' do
+    @bundle.metadata = {
+      dataverse_url: 'https://demo.dataverse.org',
+      collection_id: nil,
+      dataset_id: nil
+    }
+    ::Configuration.repo_db.stubs(:get).returns(nil)
+    meta = Dataverse::UploadBundleConnectorMetadata.new(@bundle)
+    refute meta.select_collection?
+  end
+
+  test 'select_collection? returns false when collection_id is present' do
+    @bundle.metadata = {
+      dataverse_url: 'https://demo.dataverse.org',
+      collection_id: 'COL123',
+      dataset_id: nil,
+      auth_key: 'KEY'
+    }
+    meta = Dataverse::UploadBundleConnectorMetadata.new(@bundle)
+    refute meta.select_collection?
+  end
+
+  test 'select_collection? returns false when dataset_id is present' do
+    @bundle.metadata = {
+      dataverse_url: 'https://demo.dataverse.org',
+      collection_id: nil,
+      dataset_id: 'DS123',
+      auth_key: 'KEY'
+    }
+    meta = Dataverse::UploadBundleConnectorMetadata.new(@bundle)
+    refute meta.select_collection?
+  end
 end
